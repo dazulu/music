63efd0dce8a53bb9cd683c86e00d02efa56edaf7
Adrian Payne <adrian.payne@aida.de>
2017-09-09 16:00:08 +0200

    feat(about): add transparent down arrow border

---
+++ components/atoms/ArrowDivider.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="arrow__wrapper">
+    <div class="arrow__separator">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .arrow__wrapper {
+    position: relative;
+  }
+
+  .arrow__separator {
+    height: $padding;
+    left: 0;
+    overflow: hidden;
+    position: absolute;
+    width: 100%;
+    z-index: 5;
+    bottom: 0px;
+    display: block;
+
+
+    > div {
+      background: $pure-white;
+      display: inline-block;
+      height: $padding;
+      position: absolute;
+      top: 0;
+      width: 50%;
+
+      &:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        border-bottom: $padding solid $pure-white;
+      }
+
+      &:first-child {
+        left: -$padding;
+
+        &:after {
+          right: -$padding;
+          border-right: $padding solid transparent;
+        }
+      }
+
+      &:last-child {
+        right: -$padding;
+        
+        &:after {
+          left: -$padding;
+          border-left: $padding solid transparent;
+        }
+      }
+    }
+  }
+</style>
--- components/organisms/Intro.vue
+++ components/organisms/Intro.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class="wrapper intro">
-    <div>
-      <h2 class="heading">
-        <span class="pre">SUPPORT ME</span> <br />
-        ONLINE
-      </h2>
+  <div class="intro__wrapper">
+    <div class="wrapper intro">
       <div>
-        <a class="button button--patreon" href="https://www.patreon.com/bePatron?u=2412507">
-          BECOME A PATRON!
-        </a>
-        <a class="button button--youtube" href="https://www.youtube.com/channel/UCNYBL7n1Jk9CM3NRqapHaqw">
-          <svg class="icon--youtube" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M0 7.345c0-1.294.16-2.59.16-2.59s.156-1.1.636-1.587c.608-.637 1.408-.617 1.764-.684C3.84 2.36 8 2.324 8 2.324s3.362.004 5.6.166c.314.038.996.04 1.604.678.48.486.636 1.588.636 1.588S16 6.05 16 7.346v1.258c0 1.296-.16 2.59-.16 2.59s-.156 1.102-.636 1.588c-.608.638-1.29.64-1.604.678-2.238.162-5.6.166-5.6.166s-4.16-.037-5.44-.16c-.356-.067-1.156-.047-1.764-.684-.48-.487-.636-1.587-.636-1.587S0 9.9 0 8.605v-1.26zm6.348 2.73V5.58l4.323 2.255-4.32 2.24z"/></svg>
-          YOUTUBE!
-        </a>
+        <h2 class="heading">
+          <span class="pre">SUPPORT ME</span> <br />
+          ONLINE
+        </h2>
+        <div>
+          <a class="button button--patreon" href="https://www.patreon.com/bePatron?u=2412507">
+            BECOME A PATRON!
+          </a>
+          <a class="button button--youtube" href="https://www.youtube.com/channel/UCNYBL7n1Jk9CM3NRqapHaqw">
+            <svg class="icon--youtube" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M0 7.345c0-1.294.16-2.59.16-2.59s.156-1.1.636-1.587c.608-.637 1.408-.617 1.764-.684C3.84 2.36 8 2.324 8 2.324s3.362.004 5.6.166c.314.038.996.04 1.604.678.48.486.636 1.588.636 1.588S16 6.05 16 7.346v1.258c0 1.296-.16 2.59-.16 2.59s-.156 1.102-.636 1.588c-.608.638-1.29.64-1.604.678-2.238.162-5.6.166-5.6.166s-4.16-.037-5.44-.16c-.356-.067-1.156-.047-1.764-.684-.48-.487-.636-1.587-.636-1.587S0 9.9 0 8.605v-1.26zm6.348 2.73V5.58l4.323 2.255-4.32 2.24z"/></svg>
+            YOUTUBE!
+          </a>
+        </div>
       </div>
+      <img class="singer" src="~/assets/images/adrian-payne.png" alt="Adrian Payne" />
     </div>
-    <img class="singer" src="~/assets/images/adrian-payne.png" alt="Adrian Payne" />
+    <ArrowDivider />
   </div>
 </template>
 
 <script>
+  import ArrowDivider from '~/components/atoms/ArrowDivider.vue'
 
+  export default {
+    components: {
+      ArrowDivider
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -77,7 +86,6 @@
   .singer {
     display: none;
   }
-
   
   @media only screen and (min-width: 768px) {
     .singer {
