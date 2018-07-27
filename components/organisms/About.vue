b94f7f9b7e759b3b5c76435034b5e1a2a9e842ec
Adrian Payne <adrian.payne@aida.de>
2018-07-27 14:49:59 +0200

    feat(intro): switch embedded video

--- components/organisms/About.vue
+++ components/organisms/About.vue
@@ -9,7 +9,7 @@
 
       <div class="video">
         <div class='embed-container'>
-          <iframe src='https://www.youtube.com/embed/SGlx27Iu8xA' frameborder='0' allowfullscreen></iframe>
+          <iframe src='https://www.youtube.com/embed/kBYbkUJE53A' frameborder='0' allowfullscreen></iframe>
         </div>
       </div>
 
@@ -21,50 +21,49 @@
 </script>
 
 <style lang="scss" scoped>
-  .about__wrapper {
-    background: $pure-white;
-    padding-top: $padding * 2;
-    padding-bottom: $padding * 3;
+.about__wrapper {
+  background: $pure-white;
+  padding-top: $padding * 2;
+  padding-bottom: $padding * 3;
+}
+
+.text {
+  margin-bottom: $padding * 2;
+}
+
+.embed-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
+}
+
+.embed-container iframe,
+.embed-container object,
+.embed-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+@media only screen and (min-width: 800px) {
+  .about {
+    display: flex;
   }
 
   .text {
-    margin-bottom: $padding * 2;
+    flex: 1.3;
+    margin-right: $padding;
+    margin-bottom: 0;
   }
 
-  .embed-container {
+  .video {
     position: relative;
-    padding-bottom: 56.25%;
-    height: 0;
-    overflow: hidden;
-    max-width: 100%;
+    margin-top: 2%;
+    flex: 1;
   }
-
-  .embed-container iframe,
-  .embed-container object,
-  .embed-container embed {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  
-  @media only screen and (min-width: 800px) {
-    .about {
-      display: flex;
-    }
-
-    .text {
-      flex: 1.3;
-      margin-right: $padding;
-      margin-bottom: 0;
-    }
-
-    .video {
-      position: relative;
-      margin-top: 2%;
-      flex: 1;
-    }
-  }
+}
 </style>
